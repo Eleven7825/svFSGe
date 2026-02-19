c985d8025c3a3976adbacdd13a727938af91b0bd
eleven7825 <sc7825@nyu.edu>
2026-02-19 16:34:50 +0000

    Remove pulsatile_flow folder and restore svfsi.py for clean master merge

--- pulsatile_flow/run_post_steady.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wrapper script to run post_steady_fluid.py with pvpython
-
-PVPYTHON="/home/shiyi/ParaView-5.13.0-RC1-MPI-Linux-Python3.10-x86_64/bin/pvpython"
-
-# Run the post-processing script
-$PVPYTHON post_steady_fluid.py "$@" 2>&1 | grep -v "hwloc/linux"
